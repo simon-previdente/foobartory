08cc61783f06a5f2cf207f983763b791844421be
Simon Prévidente <hello@simon-previdente.fr>
2022-01-30 22:20:15 +0100

    Determines the next required action based on inventory

--- foobartory.py
+++ foobartory.py
@@ -1,6 +1,10 @@
 import asyncio
 from typing import List
 
+FOO_MINING_REQUEST = "foo_mining_request"
+BAR_MINING_REQUEST = "bar_mining_request"
+FOO_BAR_PROCESS_REQUEST = "foo_bar_process_request"
+FOO_BAR_SELL_REQUEST = "foo_bar_sell_request"
 BUY_ROBOT_REQUEST = "buy_robot_request"
 BUY_ROBOT_RESPONSE = "buy_robot_response"
 ROBOT_READY = "robot_ready"
@@ -12,6 +16,10 @@ class Manager:
         self.requests: asyncio.Queue[str] = asyncio.Queue()
         self.results: asyncio.Queue[str] = asyncio.Queue()
         # Bag
+        self.foo_cnt = 0
+        self.bar_cnt = 0
+        self.foobar_cnt = 0
+        self.money = 0
         self.robots: List[Robot] = []
 
     def run(self) -> None:
@@ -25,11 +33,26 @@ class Manager:
         asyncio.ensure_future(robot.run())
         print("%d robot" % len(self.robots))
 
+    def determine_next_activity(self) -> str:
+        """Determines the next required action based on inventory"""
+        if self.foo_cnt >= 6 and self.money >= 3:
+            return BUY_ROBOT_REQUEST
+        elif self.foobar_cnt >= 5:
+            return FOO_BAR_SELL_REQUEST
+        elif self.foo_cnt >= 1 and self.bar_cnt >= 1 and self.money < 3:
+            return FOO_BAR_PROCESS_REQUEST
+        elif self.foo_cnt > self.bar_cnt:
+            return BAR_MINING_REQUEST
+        else:
+            return FOO_MINING_REQUEST
+
     async def _run_until_the_goal_is_reached(self) -> None:
         while len(self.robots) < 30:
             await self.results.get()
             self.add_robot()
-            await self.requests.put(BUY_ROBOT_REQUEST)
+
+            activity_needed = self.determine_next_activity()
+            await self.requests.put(activity_needed)
         for robot in self.robots:
             robot.stop()
 
--- foobartory_test.py
+++ foobartory_test.py
@@ -1,4 +1,47 @@
+import asyncio
 import unittest
+
+from foobartory import (
+    BAR_MINING_REQUEST,
+    BUY_ROBOT_REQUEST,
+    BUY_ROBOT_RESPONSE,
+    FOO_BAR_PROCESS_REQUEST,
+    FOO_BAR_SELL_REQUEST,
+    FOO_MINING_REQUEST,
+    Manager,
+    Robot,
+)
+
+
+class TestManager(unittest.TestCase):
+    def test_next_action_should_be_to_mine_foo(self):
+        manager = Manager()
+        assert manager.determine_next_activity() == FOO_MINING_REQUEST
+        assert manager.foo_cnt == 0  # no foo yet
+
+    def test_next_action_should_be_to_mine_bar(self):
+        manager = Manager()
+        manager.foo_cnt = 1
+        assert manager.determine_next_activity() == BAR_MINING_REQUEST
+        assert manager.foo_cnt == 1  # should not be changed
+        assert manager.bar_cnt == 0  # no bar yet
+
+    def test_next_action_should_be_to_process_foo_bar(self):
+        manager = Manager()
+        manager.foo_cnt = 1
+        manager.bar_cnt = 1
+        assert manager.determine_next_activity() == FOO_BAR_PROCESS_REQUEST
+
+    def test_next_action_should_be_to_sell_foo_bar(self):
+        manager = Manager()
+        manager.foobar_cnt = 5
+        assert manager.determine_next_activity() == FOO_BAR_SELL_REQUEST
+
+    def test_next_action_should_be_to_buy_robot(self):
+        manager = Manager()
+        manager.foo_cnt = 6
+        manager.money = 3
+        assert manager.determine_next_activity() == BUY_ROBOT_REQUEST
 
 
 if __name__ == "__main__":
